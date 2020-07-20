54e205a43ae3dd39b480958ba9d8fec04b990662
arle.chin <marcuradu.will.wow@gmail.com>
2020-07-20 18:52:55 +0200

    Added layout for risk metrics.

--- Hitlist Unity Project/Assets/Scripts/CardSystem/Card_ClaimReward.cs
+++ Hitlist Unity Project/Assets/Scripts/CardSystem/Card_ClaimReward.cs
@@ -9,9 +9,23 @@ public class Card_ClaimReward : SkrptrAction
     {
         Card card = GetComponentInParent<Card>();
         card.stage = CardStage.Testing;
+
+        if(card.milestone)
+        {
+            HitListMain.Instance.panelManageSoldier.balance += 150;
+            for (int i = 0; i < 3; i++)
+            {
+                HitListMain.Instance.panelManageSoldier.BuyChicken();
+            }
+        }
+        else
+        {
+            HitListMain.Instance.panelManageSoldier.balance += 50;
+        }
+        HitListMain.Instance.panelManageSoldier.SaveSoldier();
+        HitListMain.Instance.panelManageSoldier.UpdateBalances();
         card.GetComponent<SkrptrElement>().Lock();
         StartCoroutine(nameof(DeactiveGO), card);
-
     }
     public IEnumerator DeactiveGO(Card card)
     {
