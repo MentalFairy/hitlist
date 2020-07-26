ad9a4d2c5d117be5a88f59ea306a3e241f6312f2
arle.chin <marcuradu.will.wow@gmail.com>
2020-07-26 22:39:04 +0200

    Added sounds.

---
+++ Hitlist Unity Project/Assets/Skritpr/Scripts/SktrptrAnim/SkrptrAnimPlaySound.cs
@@ -0,0 +1,22 @@
+﻿using Skrptr;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkrptrAnimPlaySound : SkrptrAnim
+{
+    public List<AnimData> animDataAudio;
+    public override void Execute()
+    {
+        if (Time.realtimeSinceStartup > 3)
+        {
+            for (int i = 0; i < animDataAudio.Count; i++)
+            {
+                if (animDataAudio[i].target.GetComponent<AudioSource>() != null)
+                {
+                    animDataAudio[i].target.GetComponent<AudioSource>().PlayDelayed(animDataAudio[i].delay);
+                }
+            }
+        }
+    }
+}
