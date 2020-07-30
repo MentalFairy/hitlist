b6fd4526e6121808e468b2e1d3c23a6853374ac9
arle.chin <marcuradu.will.wow@gmail.com>
2020-07-30 23:50:45 +0200

    Added anim + more fixes.

---
+++ Hitlist Unity Project/Assets/Skritpr/Scripts/SktrptrAnim/Skrptr_DisableGO.cs
@@ -0,0 +1,35 @@
+﻿using Skrptr;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skrptr_DisableGO : SkrptrAnim
+{
+    public AnimData[] disableAnimData;
+    public override void Execute()
+    {
+        StartCoroutine(nameof(DisableGO));
+    }
+    public IEnumerator DisableGO()
+    {
+        bool foundActiveGO;
+        float timePassed = 0;
+        do
+        {
+            timePassed += Time.deltaTime;
+            foreach (var item in disableAnimData)
+            {
+                if (item.delay < timePassed)
+                    item.target.SetActive(false);
+            }
+            foundActiveGO = false;
+            foreach (var item in disableAnimData)
+            {
+                if (item.target.activeSelf)
+                    foundActiveGO = true;
+            }
+            yield return null;
+
+        } while (foundActiveGO);
+    }
+}
